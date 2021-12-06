b7cd63e6c94018569caa7af2177e3babf0ba1864
yuxiang.feng <yuxiang.feng@sqmall.com>
2021-12-06 21:15:06 +0800

    fix algorithm,and compare bubbleSort excute time

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import java.util.Arrays;
+
 /**
  * 排序
  * @author yuxiangfeng
@@ -9,36 +11,83 @@ public class Sort {
     private static int TWO = 2;
 
     public static void main(String[] args) {
+        compareBubbleSort(100,10000,100);
+        compareBubbleSort(100,10000,500);
+        compareBubbleSort(100,10000,1000);
+        compareBubbleSort(100,10000,2000);
+    }
 
-        int[] arr1 = getIntList(0,2000,2000);
+    public static void compareBubbleSort(int min,int max,int num){
+        int[] arr1 = getIntList(min,max,num);
         int[] arr2 =arr1.clone();
-        printList(arr1);
-        Long startTime =  System.currentTimeMillis();
-        bubbleSort(arr1);
-        Long endTime =  System.currentTimeMillis();
-        printList(arr1);
-        System.out.println("执行时间为：" + (endTime - startTime));
+        int[] arr3 =arr1.clone();
+        int[] arr4 =arr1.clone();
 
-        System.out.println("++++++++++++++++++++");
-        printList(arr2);
+        Long startTime1 =  System.currentTimeMillis();
+        bubbleSortV1(arr1);
+        Long endTime1 =  System.currentTimeMillis();
+
         Long startTime2 =  System.currentTimeMillis();
-        bubbleSort2(arr1);
+        bubbleSortV2(arr2);
         Long endTime2 =  System.currentTimeMillis();
-        printList(arr1);
-        System.out.println("执行时间为：" + (endTime2 - startTime2));
+
+        Long startTime3 =  System.currentTimeMillis();
+        bubbleSortV3(arr3);
+        Long endTime3 =  System.currentTimeMillis();
+
+        Long startTime4 =  System.currentTimeMillis();
+        bubbleSortV4(arr4);
+        Long endTime4 =  System.currentTimeMillis();
+
+        if (!Arrays.equals(arr1,arr2) || !Arrays.equals(arr1,arr3) || !Arrays.equals(arr1,arr4)){
+            System.out.println("error");
+        }else {
+            Long executeTime1 = endTime1 - startTime1;
+            Long executeTime2 = endTime2 - startTime2;
+            Long executeTime3 = endTime3 - startTime3;
+            Long executeTime4 = endTime4 - startTime4;
+
+            System.out.println("v1、v2、v3、v4执行时间分别为：\t"
+                    + executeTime1 + '\t' + executeTime2
+                    + '\t' + executeTime3 + '\t' + executeTime4);
+
+            Long[] executeTimes = {executeTime1,executeTime2,executeTime3,executeTime4};
+            bubbleSort(executeTimes);
+            printList(executeTimes,"executeTimes");
+        }
 
     }
 
 
+    public static void bubbleSort(Object[] arr){
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        if ( arr[0] instanceof Long || arr[0] instanceof Integer || arr[0] instanceof Short) {
+
+            Object temp;
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = 0; j < arr.length - 1 - i; j++) {
+                    if ((Long)arr[j] > (Long)arr[j + 1]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }else {
+            return;
+        }
+    }
 
     /**
-     * 冒泡排序
+     * 冒泡排序(博客思路)
      * 将待插元素，依次与已排序好的子数列元素从后到前进行比较，如果当前元素值比待插元素值大，
      * 则将移位到与其相邻的后一个位置，否则直接将待插元素插入当前元素相邻的后一位置，
      * 因为说明已经找到插入点的最终位置
      * @param arr
      */
-    public static void bubbleSort(int[] arr){
+    public static void bubbleSortV1(int[] arr){
         if ( arr.length >= TWO ){
             for (int i = 1; i < arr.length;i++ ){
                 int x = arr[i];
@@ -52,24 +101,71 @@ public class Sort {
         }
     }
 
-    public static void bubbleSort2(int[] arr){
-        int deep = arr.length;
-        while (deep > 1){
-            int max = arr[0];
-            for (int i = 1;i < deep;i++){
-                if (max < arr[i]){
-                    int temp = max;
-                    max = arr[i];
-                    arr[i-1] = temp;
-                }else if (max > arr[i]){
-                    arr[i-1] = arr[i];
-                }else {
-                    i++;
+    /**
+     * 标准冒泡排序，优先确定最后一个元素
+     * @param arr
+     */
+    public static void bubbleSortV2(int[] arr){
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
-            deep--;
         }
     }
+
+    /**
+     * 标准冒泡排序，优先确定第一个元素
+     * @param arr
+     */
+    public static void bubbleSortV3(int[] arr){
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = i + 1; j < arr.length ; j++) {
+                if (arr[i] > arr[j]){
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 一次比较两个数的大小，将小数的下标记录在min中，遍历一次后，
+     * 将第i个数和第min个数进行交换
+     * @param arr
+     */
+    public static void bubbleSortV4(int[] arr){
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int min;
+        for (int i = 0; i < arr.length - 1; i++){
+            min=i;
+            for (int j = i + 1; j < arr.length ; j++) {
+                if ( arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            if (min != i){
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
 
     /**
      * 分治排序、快速排序
@@ -101,7 +197,6 @@ public class Sort {
 
 
 
-
     /**
      * 获取int数组
      * @param min
@@ -121,13 +216,13 @@ public class Sort {
      * 打印数组
      * @param arr
      */
-    public static void printList(int[] arr){
-        System.out.println("======> print start:");
-        for (int i:arr){
+    public static void printList(Object[] arr,String otherPrint){
+        System.out.println(otherPrint);
+        for (Object i:arr){
             System.out.print(i + "\t");
         }
         System.out.println();
-        System.out.println("print over");
+//        System.out.println("print over");
     }
 
 }
